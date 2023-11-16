cc55c8d7b993ba5a2be9b02f899adaff3cc76a62
Lucas Fischer <lucas.fischer2002@t-online.de>
2023-11-16 19:03:36 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import MainLayout from "./components/Layout/MainLayout";
+import { Routes, Route } from "react-router-dom";
+import TaskList from "./components/TaskList";
 
 const App = () => {
   return (
     <>
       <MainLayout>
-        <div className="content"></div>
+        <div className="content">
+          <Routes>
+            <Route index element={<TaskList />} />
+            <Route path="/important" element={<h1>Important</h1>} />
+            <Route path="/completed" element={<h1>Completed</h1>} />
+            <Route path="/incompleted" element={<h1>Incompleted</h1>} />
+          </Routes>
+        </div>
       </MainLayout>
     </>
   );
